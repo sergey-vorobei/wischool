f4f999d19e1ceb9cdaba56dc894511b597bb0297
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-11-14 13:17:29 +0200

    Added responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,41 @@
-// eslint-disable-next-line strict
-window.addEventListener('DOMContentLoaded', (event) => {
-  const slider = document.querySelector('.categories__cards-wrapper');
-  const cardsContainer = document.querySelector('.cards__container');
-  const cards = slider.querySelectorAll('.categories__card');
-  const nextButton = document.querySelector('.categories__slider-btn--next');
-  const prevButton = document.querySelector('.categories__slider-btn--prev');
-  const cardWidth = cards[0].getBoundingClientRect().width;
-  const gap = 32;
-  let offset = 0;
+'use strict';
 
-  updateSlider();
+/* global Swiper */
 
-  nextButton.addEventListener('click', function() {
-    if (offset < (cards.length - 4) * (cardWidth + gap)) {
-      offset += cardWidth + gap;
-      updateSlider();
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  // eslint-disable-next-line no-new
+  new Swiper('.categories__swiper', {
+    navigation: {
+      nextEl: '.categories__slider-btn--next',
+      prevEl: '.categories__slider-btn--prev',
+    },
+    autoHeight: true,
+    slidesPerView: 4,
+    watchOverflow: true,
+    spaceBetween: 32,
+    slidesPerGroup: 1,
+    height: 400,
   });
-
-  prevButton.addEventListener('click', function() {
-    if (offset > 0) {
-      offset -= cardWidth + gap;
-      updateSlider();
-    }
-  });
-
-  function updateSlider() {
-    cardsContainer.style.transform = `translateX(-${offset}px)`;
-    checkButtons();
-  }
-
-  function checkButtons() {
-    prevButton.disabled = offset === 0;
-    nextButton.disabled = offset >= (cards.length - 4) * (cardWidth + gap);
-  }
 });
 
+// Mobile menu
 const nav = document.querySelector('.nav');
 const burger = document.querySelector('#burger');
 const cross = document.querySelector('#nav-cross');
+const navItems = document.querySelectorAll('.nav__item');
 
-burger.addEventListener('click', function() {
+function toggleMobMenu() {
   nav.classList.toggle('header__nav--active');
-});
+}
 
-cross.addEventListener('click', function() {
-  nav.classList.toggle('header__nav--active');
+burger.addEventListener('click', toggleMobMenu);
+
+cross.addEventListener('click', toggleMobMenu);
+
+function closeMobileMenu() {
+  nav.classList.remove('header__nav--active');
+}
+
+navItems.forEach(function(item) {
+  item.addEventListener('click', closeMobileMenu);
 });
